02884f4a57c33f73fafaa3487f8e9f54ad1d8a09
AlexandrMay <maysalexandr@gmail.com>
2019-06-16 17:42:27 +0300

    first

---
+++ requests/weather.request.js
@@ -0,0 +1,37 @@
+const rp = require('request-promise')
+
+module.exports = async function(city = '') {
+    if(!city){
+        throw new Error('Invalid city')
+    }
+
+    const KEY = 'c4f054675de2fceb991756ab0e5b78c0'
+    const uri = 'http://api.openweathermap.org/data/2.5/weather'
+
+    const options = {
+        uri,
+        qs: {
+            appid: KEY,
+            q: city,
+            units: 'imperial'
+        },
+        json: true
+    }
+
+    try {
+        const data = await rp(options)
+        const celsius = (data.main.temp - 32)*5/9
+        return {
+            weather: `${data.name}: ${celsius.toFixed(0)}`,
+            error: null
+        }  
+    } catch (error) {
+        return{
+            weather: null,
+            error: error.error.message
+        }
+    }
+
+    
+
+}
